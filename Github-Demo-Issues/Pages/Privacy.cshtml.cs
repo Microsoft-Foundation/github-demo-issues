7095de3f17de5b32bdcc42bb317fc64f018cb52d
Vinod Soni <vinod.soni@microsoft.com>
2023-04-20 12:54:38 -0400

    Add project files.

---
+++ Github-Demo-Issues/Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Github_Demo_Issues.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
